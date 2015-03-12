4bff3ad711378e723427fa92d062efdf9bfd5ac9
vfeltzin <susanne392>
2015-03-12 12:13:52 -0400

    Fixed a mistake in argument parser, added hist fn

--- hiss.py
+++ hiss.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
 import argparse
+import matplotlib.pyplot as plt
 
 def parse_command_line_input():
 	parser = argparse.ArgumentParser()
-	parser.add_argument(arg[],'--input_filepath', help='Path to the input file with forward reads', required=True)
+	parser.add_argument('-i','--input_filepath', help='Path to the input file with forward reads', required=True)
 	command_line_args=parser.parse_args()	
 	return command_line_args
 
+def make_hist(x):
+	plt.figure(num=1)
+	plt.hist(x,bins=10)
+	plt.show()
