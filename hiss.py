ee7891b78907ba83fe8369be8525e647ebe5e205
vfeltzin <susanne392>
2015-03-12 11:43:26 -0400

    Added command line argument parser

---
+++ hiss.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import argparse
+
+def parse_command_line_input():
+	parser = argparse.ArgumentParser()
+	parser.add_argument(arg[],'--input_filepath', help='Path to the input file with forward reads', required=True)
+	command_line_args=parser.parse_args()	
+	return command_line_args
+
