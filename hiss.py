57b57b95d5cfda8745b52f5cf217521dc18081ea
vfeltzin <susanne392>
2015-03-13 16:08:56 -0400

    Concatenating tables is now optional
    I want the user to have the following input options: 1) one histogram
    from one spreadsheet, one histogram from multiple spreadsheets,
    multiple histograms from multiple spreadsheets, multiple histograms
    from one spreadsheet.

--- hiss.py
+++ hiss.py
@@ -15,12 +15,17 @@ def make_hist(x):
 	plt.hist(x,bins=10)
 	plt.show()
 	
-def load_data_from_file(filepaths):
+#Input options: 1) one histogram from one spreadsheet, one histogram from multiple spreadsheets, multiple histograms from multiple spreadsheets, multiple histograms from one spreadsheet.
+
+def load_data_from_file(filepaths,concatenate):
 	input_data = []
 	for filepath in filepaths:
 		input_data.append(pd.read_csv(filepath,sep='[\t,]',engine='python'))
-	data = (pd.concat(input_data,ignore_index=True))
+	if concatenate:
+		input_data = (pd.concat(input_data,ignore_index=True))
+	return input_data
 	
 filepath = "/Users/lekova/Desktop/dl1.mirnas.mature.ratio.txt"
 filepath2 = "/Users/lekova/Desktop/dl1.mirnas.mature.ratio.csv"
-load_data_from_file([filepath,filepath2])
+input_data = load_data_from_file([filepath,filepath2],concatenate=False)
+print(input_data)
