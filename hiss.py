be770793d615a4c869da341d67b2b24ac6366c97
vfeltzin <susanne392>
2015-03-13 18:33:41 -0400

    Added main(), hist generator fn

--- hiss.py
+++ hiss.py
@@ -10,10 +10,13 @@ def parse_command_line_input():
 	command_line_args=parser.parse_args()	
 	return command_line_args
 
-def make_hist(x):
-	plt.figure(num=1)
-	plt.hist(x,bins=10)
-	plt.show()
+def make_hist(n,input_data,Xcolname,bins):
+	curr_num=0
+	while curr_num<n:
+		afig = input_data[curr_num][Xcolname].hist(bins=bins)
+		yield afig
+		curr_num=curr_num+1
+		
 	
 #Input options: 1) one histogram from one spreadsheet, one histogram from multiple spreadsheets, multiple histograms from multiple spreadsheets, multiple histograms from one spreadsheet.
 
@@ -24,8 +27,23 @@ def load_data_from_file(filepaths,concatenate):
 	if concatenate:
 		input_data = (pd.concat(input_data,ignore_index=True))
 	return input_data
+
+def main():	
+	filepath = "/Users/lekova/Desktop/dl1.mirnas.mature.ratio.txt"
+	filepath2 = "/Users/lekova/Desktop/dl1.mirnas.mature.ratio.csv"
+	input_data = load_data_from_file([filepath,filepath2],concatenate=False)
+
+	Xcolname = 's5/s6'
 	
-filepath = "/Users/lekova/Desktop/dl1.mirnas.mature.ratio.txt"
-filepath2 = "/Users/lekova/Desktop/dl1.mirnas.mature.ratio.csv"
-input_data = load_data_from_file([filepath,filepath2],concatenate=False)
-print(input_data)
+	pd.options.display.mpl_style = 'default'	
+
+	bins=10
+
+	n=2
+
+	figs=list(make_hist(n,input_data,Xcolname,bins))
+	
+	print(figs)
+	
+if __name__ == '__main__':
+	main()
