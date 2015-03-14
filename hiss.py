42684dff8338a50c70652d491c2049face00bcea
vfeltzin <vfeltzin@gmail.com>
2015-03-14 18:49:36 -0400

    Fixed option to group by column
    The grouping is only called if the optional --groupbycolumn argument is
    not None

--- hiss.py
+++ hiss.py
@@ -8,36 +8,31 @@ def parse_command_line_input():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-i','--inputfiles', required=True,action='append')
 	parser.add_argument('--concatenate',action='store_true')
-	parser.add_argument('--groupbycolumn',action='store_true')
 	parser.add_argument('--Xcolname', required=True)
 	parser.add_argument('-b','--bins',type=int)
+	parser.add_argument('--groupbycolumn')
 	args = parser.parse_args()
 	return args	
 	
-#Input options: 1) one histogram from one spreadsheet, one histogram from multiple spreadsheets, multiple histograms from multiple spreadsheets, multiple histograms from one spreadsheet.
-
-def load_data_from_file(filepaths,concatenate,groupbycolumn,grouping_column):
+def load_data_from_file(filepaths,concatenate,groupbycolumn):
 	input_data = [pd.read_csv(filepath,sep='[\t,]',engine='python') for filepath in filepaths]
 	if concatenate:
 		input_data = (pd.concat(input_data,ignore_index=True))
-	if groupbycolumn:
+	if groupbycolumn!=None:
 		grouped_input_data=[]
 		for datum in input_data:
-			grouped = datum.groupby(grouping_column)
+			grouped = datum.groupby(groupbycolumn)
 			for k,gp in grouped:
 				grouped_input_data.append(gp)
 		input_data = grouped_input_data
 	return input_data
 
 def main():	
-	#filepath = "C:/Users/Virzhiniya/Desktop/test.txt"
-	#filepath2 = "C:/Users/Virzhiniya/Desktop/test.txt"
 	args = parse_command_line_input()
-	input_data = load_data_from_file(args.inputfiles,args.concatenate,args.groupbycolumn,args.grouping_column)
+	input_data = load_data_from_file(args.inputfiles, args.concatenate, args.groupbycolumn)
 	
 	print(input_data)
 	pd.options.display.mpl_style = 'default'	
-	n=2
 
 	for datum in input_data:
 		datum[args.Xcolname].hist(bins=args.bins)
